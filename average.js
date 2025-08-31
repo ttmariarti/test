481f0170aa332c25455c89a8b17bfb1b706061cb
ttmariarti <dupinm21@gmail.com>
2025-08-31 11:13:05 +0300

    cssdsd

---
+++ average.js
@@ -0,0 +1,7 @@
+export function average(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum / numbers.length;
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+import { average } from './average.js';
+
+const numbers = [1, 2, 3, 4, 5];
+const avg = average(numbers);
+console.log(`Average: ${avg}`);
